8d13601cdf5f573c75b6a41acdacc4ea6a169723
Sebastian Guevara <johanguevara1221@gmail.com>
2024-04-01 02:13:37 -0300

    Portfolio 1st Version

---
+++ src/consts/navigation.ts
@@ -0,0 +1,32 @@
+export const nav = [
+    {
+      title: "Home",
+      label: "home",
+      url: "/#home",
+      icon: "Logo",
+    },
+    {
+      title: "Experience",
+      label: "experience",
+      url: "/#experience",
+      icon: "Briefcase",
+    },
+    {
+      title: "Projects",
+      label: "projects",
+      url: "/#projects",
+      icon: "FileCode",
+    },
+    {
+      title: "Education",
+      label: "education",
+      url: "/#education",
+      icon: "School",
+    },
+    {
+      title: "About",
+      label: "about",
+      url: "/#about",
+      icon: "About",
+    },
+  ];
